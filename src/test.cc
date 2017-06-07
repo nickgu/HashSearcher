68695d129e23ad5ffb213cad924b570f949c1834
斯缪 <simiu_0609@163.com>
2017-06-07 15:04:57 +0800

    change code

--- src/test.cc
+++ src/test.cc
@@ -39,11 +39,73 @@ void Test_DiffBit() {
     debug_show_i64(a ^ b);
 
     printf("DiffBit: %d\n", dict.diffbit(a,b));
+    printf("DiffBit: %d\n", __builtin_popcountl(a^b));
 }
+
+void Test_DoubleCheck() {
+    const size_t Num = 30000000;
+    size_t QueryCount = 20;
+    int DiffBit = 7;
+
+    BEGIN;
+    size_t *hashs = new size_t [Num];
+    for (size_t i=0; i<Num; ++i) {
+        // this is compact code.
+        //hashs[i] = (size_t) i;
+        hashs[i] = random_i64();
+    }
+    SHOWDIFFTIME(random);
+
+    // build.
+    BEGIN;
+    HashSearcher dict;
+    dict.build(hashs, Num);
+    SHOWDIFFTIME(build);
+
+    // search.
+    BEGIN;
+    int current_count = 0;
+    for (int i=0; i<QueryCount; ++i) {
+        size_t query = random_i64();
+        vector<size_t> search_ans = dict.search(query, DiffBit);
+        sort(search_ans.begin(), search_ans.end());
+
+
+        vector<size_t> raw_ans;
+        for (int i=0; i<Num; ++i) {
+            if (dict.diffbit(hashs[i], query) <= DiffBit) {
+                raw_ans.push_back(hashs[i]);
+            }
+        }
+        sort(raw_ans.begin(), raw_ans.end());
+
+        fprintf(stderr, "query [%s] search_ans=%lu raw_ans=%lu\n", bit_string(query).c_str(), search_ans.size(), raw_ans.size());
+        if (raw_ans.size() == search_ans.size()) {
+            bool all_same = true;
+            for (size_t i=0; i<raw_ans.size(); ++i) {
+                if (raw_ans[i] != search_ans[i]) {
+                    all_same = false;
+                    break;
+                }
+            }
+            if ( all_same ) {
+                current_count += 1;
+            }
+        }
+    }
+    SHOWDIFFTIME(in_dict_search);
+
+    fprintf(stderr, "query : %lu\n", QueryCount);
+    fprintf(stderr, "current count : %d\n", current_count);
+
+    delete [] hashs;
+    return ;
+}
+
 
 void Test_3000w() {
     const size_t Num = 30000000;
-    size_t query_count = 100000;
+    size_t QueryCount = 100000;
 
     BEGIN;
     size_t *hashs = new size_t [Num];
@@ -64,7 +126,7 @@ void Test_3000w() {
     BEGIN;
     size_t ans_count = 0;
     size_t perf_count = 0;
-    for (int i=0; i<query_count; ++i) {
+    for (int i=0; i<QueryCount; ++i) {
         size_t query = hashs[random() % Num];
         vector<size_t> ans = dict.search(query, 7);
         ans_count += ans.size();
@@ -72,10 +134,10 @@ void Test_3000w() {
         perf_count += dict.get_performance_counter();
     }
     SHOWDIFFTIME(in_dict_search);
-    fprintf(stderr, "query : %lu\n", query_count);
-    fprintf(stderr, "ans / query : %.2f\n", ans_count * 1.0 / query_count);
-    fprintf(stderr, "perf / query : %.2f\n", perf_count * 1.0 / query_count);
-    fprintf(stderr, "qps : %.2f\n", query_count * 1.0 / difft);
+    fprintf(stderr, "query : %lu\n", QueryCount);
+    fprintf(stderr, "ans / query : %.2f\n", ans_count * 1.0 / QueryCount);
+    fprintf(stderr, "perf / query : %.2f\n", perf_count * 1.0 / QueryCount);
+    fprintf(stderr, "qps : %.2f\n", QueryCount * 1.0 / difft);
 
     delete [] hashs;
     return ;
@@ -83,11 +145,14 @@ void Test_3000w() {
 
 
 int main() {
+
     //srand(19870609);
     srand((int)time(0));
 
+    Test_DoubleCheck();
+
     //Test_DiffBit();
 
-    Test_3000w();
+    //Test_3000w();
     return 0;
 }
